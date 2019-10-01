c907df5918367ab29d4c0d58bf396f4f09a87746
AstroKD <kdreyerinseattle@gmail.com>
2019-10-01 14:35:26 -0700

    added bio and started js

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 'use strict';
+// Greeting section
+var userNme = prompt('Hi, What is your name?');
+if ((userNme === null)||(userNme === '')) {userNme = 'No Name';}
+console.log(userNme);
+alert('Greetings ' + userNme + ', Welcome to Kevin\'s About Me page');
 
-console.log('Im alive');
+// Quiz section
+alert('Please take this short quiz about me, answer yes or no.')
+var GrewUp = prompt('Quiz Question 1: Did Kevin grow up in New Jersey?');
+GrewUp = GrewUp.toLowerCase();
 
-
-var likesStrangerThings = prompt('Hello, do you like Stranger Things on Netflix');
-likesStrangerThings = likesStrangerThings.toLowerCase();
-
-if (likesStrangerThings === 'yes' || likesStrangerThings === 'y') {
-  alert('Yah me too!');
+if (GrewUp === 'yes' || GrewUp === 'y') {
+  // alert('Correct, I grew up in New Jersey, just outside of Philadelphia PA.');
+  console.log('q1: correct');
 } else {
-  alert('Go watch it!');
+  console.log('q1: wrong');
 }
+
 
 
